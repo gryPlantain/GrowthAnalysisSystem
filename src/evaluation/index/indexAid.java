79fe84c8c386369470727dfb09f4db154583480b
guanru <guranruyang@yeah.net>
2015-01-11 16:06:14 +0800

    IndexAid getter and setter

--- src/evaluation/index/indexAid.java
+++ src/evaluation/index/indexAid.java
@@ -4,5 +4,191 @@ public class indexAid {
 	private float begTotalAssets; 	//期初资产总计（期初总资产）
 	private float finTotalAssets;	//期末资产总计（期末总资产）
 	private float netProfit;			//净利润
+	private float begTotalDebt;		//期初总负债（期初负债合计）
+	private float finTotalDebt;		//期末总资产（期末负债合计）
+	private float optIcome;			//营业收入
+	private float optCosts;			//营业成本
+	private float dilutedEarn;		//稀释每股收益
+	private float begCurAssets;		//期初流动资产合计
+	private float finCurAssets;		//期末流动资产合计
+	private float begCurDebt;			//期初流动负债
+	private float finCurDebt;			//期末流动负债
+	private float begInventory;		//期初存货；
+	private float finIventory;		//期末存货
+	private float moneyFunds;			//货币资金
+	private float anualInvestment;	//一年内到期的非流动资产
+	private float begReceivables;	//期初应收帐款
+	private float finReceivables;	//期末应收帐款
+	private float totalProfit;		//利润总额
+	private float sellingCosts;		//销售费用	
+	private float manageCosts;		//管理费用
+	private float mainBizIncome;		//主营业务收入
+	private float begOwnerInterests;//期初所有者权益
+	private float finOwnerInterests;//期末所有者权益
+	private float begBizIncome;		//期初营业收入
+	private float finBizIncome;		//期末营业收入
+	private float intagibleAssets;	//无形资产
+	public float getBegTotalAssets() {
+		return begTotalAssets;
+	}
+	public void setBegTotalAssets(float begTotalAssets) {
+		this.begTotalAssets = begTotalAssets;
+	}
+	public float getFinTotalAssets() {
+		return finTotalAssets;
+	}
+	public void setFinTotalAssets(float finTotalAssets) {
+		this.finTotalAssets = finTotalAssets;
+	}
+	public float getNetProfit() {
+		return netProfit;
+	}
+	public void setNetProfit(float netProfit) {
+		this.netProfit = netProfit;
+	}
+	public float getBegTotalDebt() {
+		return begTotalDebt;
+	}
+	public void setBegTotalDebt(float begTotalDebt) {
+		this.begTotalDebt = begTotalDebt;
+	}
+	public float getFinTotalDebt() {
+		return finTotalDebt;
+	}
+	public void setFinTotalDebt(float finTotalDebt) {
+		this.finTotalDebt = finTotalDebt;
+	}
+	public float getOptIcome() {
+		return optIcome;
+	}
+	public void setOptIcome(float optIcome) {
+		this.optIcome = optIcome;
+	}
+	public float getOptCosts() {
+		return optCosts;
+	}
+	public void setOptCosts(float optCosts) {
+		this.optCosts = optCosts;
+	}
+	public float getDilutedEarn() {
+		return dilutedEarn;
+	}
+	public void setDilutedEarn(float dilutedEarn) {
+		this.dilutedEarn = dilutedEarn;
+	}
+	public float getBegCurAssets() {
+		return begCurAssets;
+	}
+	public void setBegCurAssets(float begCurAssets) {
+		this.begCurAssets = begCurAssets;
+	}
+	public float getFinCurAssets() {
+		return finCurAssets;
+	}
+	public void setFinCurAssets(float finCurAssets) {
+		this.finCurAssets = finCurAssets;
+	}
+	public float getBegCurDebt() {
+		return begCurDebt;
+	}
+	public void setBegCurDebt(float begCurDebt) {
+		this.begCurDebt = begCurDebt;
+	}
+	public float getFinCurDebt() {
+		return finCurDebt;
+	}
+	public void setFinCurDebt(float finCurDebt) {
+		this.finCurDebt = finCurDebt;
+	}
+	public float getBegInventory() {
+		return begInventory;
+	}
+	public void setBegInventory(float begInventory) {
+		this.begInventory = begInventory;
+	}
+	public float getFinIventory() {
+		return finIventory;
+	}
+	public void setFinIventory(float finIventory) {
+		this.finIventory = finIventory;
+	}
+	public float getMoneyFunds() {
+		return moneyFunds;
+	}
+	public void setMoneyFunds(float moneyFunds) {
+		this.moneyFunds = moneyFunds;
+	}
+	public float getAnualInvestment() {
+		return anualInvestment;
+	}
+	public void setAnualInvestment(float anualInvestment) {
+		this.anualInvestment = anualInvestment;
+	}
+	public float getBegReceivables() {
+		return begReceivables;
+	}
+	public void setBegReceivables(float begReceivables) {
+		this.begReceivables = begReceivables;
+	}
+	public float getFinReceivables() {
+		return finReceivables;
+	}
+	public void setFinReceivables(float finReceivables) {
+		this.finReceivables = finReceivables;
+	}
+	public float getTotalProfit() {
+		return totalProfit;
+	}
+	public void setTotalProfit(float totalProfit) {
+		this.totalProfit = totalProfit;
+	}
+	public float getSellingCosts() {
+		return sellingCosts;
+	}
+	public void setSellingCosts(float sellingCosts) {
+		this.sellingCosts = sellingCosts;
+	}
+	public float getManageCosts() {
+		return manageCosts;
+	}
+	public void setManageCosts(float manageCosts) {
+		this.manageCosts = manageCosts;
+	}
+	public float getMainBizIncome() {
+		return mainBizIncome;
+	}
+	public void setMainBizIncome(float mainBizIncome) {
+		this.mainBizIncome = mainBizIncome;
+	}
+	public float getBegOwnerInterests() {
+		return begOwnerInterests;
+	}
+	public void setBegOwnerInterests(float begOwnerInterests) {
+		this.begOwnerInterests = begOwnerInterests;
+	}
+	public float getFinOwnerInterests() {
+		return finOwnerInterests;
+	}
+	public void setFinOwnerInterests(float finOwnerInterests) {
+		this.finOwnerInterests = finOwnerInterests;
+	}
+	public float getBegBizIncome() {
+		return begBizIncome;
+	}
+	public void setBegBizIncome(float begBizIncome) {
+		this.begBizIncome = begBizIncome;
+	}
+	public float getFinBizIncome() {
+		return finBizIncome;
+	}
+	public void setFinBizIncome(float finBizIncome) {
+		this.finBizIncome = finBizIncome;
+	}
+	public float getIntagibleAssets() {
+		return intagibleAssets;
+	}
+	public void setIntagibleAssets(float intagibleAssets) {
+		this.intagibleAssets = intagibleAssets;
+	}
 	
 }
